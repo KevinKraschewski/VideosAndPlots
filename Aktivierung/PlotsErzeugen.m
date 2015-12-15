bd7eef24ff6878549b704eaa6142ac1d9b5619ee
KevinKraschewski <kevin.kraschewski@web.de>
2015-12-15 21:25:23 +0100

    Angepasst
    Nothing special

--- Aktivierung/PlotsErzeugen.m
+++ Aktivierung/PlotsErzeugen.m
@@ -1,47 +1,81 @@
 %% Erzeugt den 3-D Plot ueber Frequenz/Aktivierung und resultierende Differenz der maximalen Amplituden
-
 % Die Frequenzen fuer die simuliert wurden
-frequencies = 50:-5:15;
+frequencies = [120:-5:95 90:-2.5:60];
 % Die Aktivierungen fuer die simuliert wurden
 activations = 1:-.05:.05;
 
 [X,Y] = meshgrid(activations,frequencies);
 
-Amp = zeros(20,8);
-for i = 4  : 11
+Amp = zeros(20,6+13);
+for i = 1  : 6
     % Lade die Daten
-    load(['C:\Users\Kevin Kraschewski\Documents\MATLAB\HiWi\SmallActivationChange' num2str(i) '_12mm.mat'])
+    load(['SmallActivationChange120to60' num2str(i) '_12mm.mat'])
     
     
     for j = 1 : 20
         t = SimResults_Time{j};
         y = SimResults_Y{j};
         c = Config{j};
-              
-        [~,HelpAmpMaxima,~,~] = c.getOutputOfInterest(t,y);
-
+        
+        [~,~,~,HelpAmpMaxima] = c.getOutputOfInterest(t,y);
+        
         
         % Sollte der Muskel noch nicht aktiviert worden sein wg Simulation
         % "entferne" diesen Wert aus den Daten zusammen mit der
         % Frequenz/Aktivierung
         
         if isnan(HelpAmpMaxima)
-            X(i-3,j) = NaN;
-            Y(i-3,j) = NaN;
+            X(i,j) = NaN;
+            Y(i,j) = NaN;
             
         end
         % Maximum der Maxima -> vermutlich immer die Mitte
-        Amp(j,i-3) = max(HelpAmpMaxima);
+        Amp(j,i) = max(HelpAmpMaxima);
     end
     
 end
 
+for i = 1  : 13
+    % Lade die Daten
+    load(['SmallActivationChange90to60' num2str(i) '_12mm.mat'])
+    
+    
+    for j = 1 : 20
+        t = SimResults_Time{j};
+        y = SimResults_Y{j};
+        c = Config{j};
+        
+        [~,~,~,HelpAmpMaxima] = c.getOutputOfInterest(t,y);
+        
+        
+        % Sollte der Muskel noch nicht aktiviert worden sein wg Simulation
+        % "entferne" diesen Wert aus den Daten zusammen mit der
+        % Frequenz/Aktivierung
+        
+        if isnan(HelpAmpMaxima)
+            X(i+6,j) = NaN;
+            Y(i+6,j) = NaN;
+            
+        end
+        % Maximum der Maxima -> vermutlich immer die Mitte
+        Amp(j,i+6) = max(HelpAmpMaxima);
+    end
+    
+end
 
-% Keine Ahnung wo die anderen herkamen?!
-Amp = Amp(1:20,1:8);
 %% Grid und Surfplot
+close all
+figure
 surf(X,Y,Amp')
 title('Differenz der maximalen Amplituden in Abhaengigkeit von Frequenz und maximaler Aktivierung');
 ylabel('Frequenzen [Hz]');
 xlabel('maximale Aktivierung [%]');
-zlabel('maximale Amplitudendifferenz [mm]');
+zlabel('maximale Amplitudenverstärkung [%]');
+
+figure
+[maxAmpsPerActivation,Indices] = max(Amp');
+Resonanzfrequenz = frequencies(Indices);
+plot(activations,Resonanzfrequenz);
+xlabel('maximale Aktivierung [%]');
+ylabel('Resonanzfrequenz [Hz]');
+title('Resonanzfrequenzen über Aktivierung');
--- Aktivierung/PlotsTimeShift.m
+++ Aktivierung/PlotsTimeShift.m
@@ -1,44 +1,68 @@
 %% Erzeugt den 3-D Plot ueber Frequenz/Aktivierung und resultierende Differenz der maximalen Amplituden
 
-% Die Frequenzen fuer die simuliert wurden
-frequencies = 50:-5:15;
+frequencies = [120:-5:95 90:-2.5:60];
 % Die Aktivierungen fuer die simuliert wurden
 activations = 1:-.05:.05;
 
 [X,Y] = meshgrid(activations,frequencies);
 
-TimeShifts = zeros(20,8);
-for i = 4  : 11
+TimeShifts = zeros(20,6+13);
+for i = 1  : 6
     % Lade die Daten
-    load(['C:\Users\Kevin Kraschewski\Documents\MATLAB\HiWi\SmallActivationChange' num2str(i) '_12mm.mat'])
+    load(['SmallActivationChange120to60' num2str(i) '_12mm.mat'])
     
     
     for j = 1 : 20
         t = SimResults_Time{j};
         y = SimResults_Y{j};
         c = Config{j};
-              
-        [TimeShift,~,~,~] = c.getOutputOfInterest(t,y);
-
+        
+        [HelpTimeShifts,~,~,~] = c.getOutputOfInterest(t,y);
+        
         
         % Sollte der Muskel noch nicht aktiviert worden sein wg Simulation
         % "entferne" diesen Wert aus den Daten zusammen mit der
         % Frequenz/Aktivierung
         
-        if isnan(TimeShift)
-            X(i-3,j) = NaN;
-            Y(i-3,j) = NaN;
+        if isnan(HelpTimeShifts)
+            X(i,j) = NaN;
+            Y(i,j) = NaN;
             
         end
         % Maximum der Maxima -> vermutlich immer die Mitte
-        TimeShifts(j,i-3) = TimeShift;
+        TimeShifts(j,i) = max(HelpTimeShifts);
     end
     
 end
 
+for i = 1  : 13
+    % Lade die Daten
+    load(['SmallActivationChange90to60' num2str(i) '_12mm.mat'])
+    
+    
+    for j = 1 : 20
+        t = SimResults_Time{j};
+        y = SimResults_Y{j};
+        c = Config{j};
+        
+        [~,~,~,HelpTimeShifts] = c.getOutputOfInterest(t,y);
+        
+        
+        % Sollte der Muskel noch nicht aktiviert worden sein wg Simulation
+        % "entferne" diesen Wert aus den Daten zusammen mit der
+        % Frequenz/Aktivierung
+        
+        if isnan(HelpTimeShifts)
+            X(i+6,j) = NaN;
+            Y(i+6,j) = NaN;
+            
+        end
+        % Maximum der Maxima -> vermutlich immer die Mitte
+        TimeShifts(j,i+6) = max(HelpTimeShifts);
+    end
+    
+end
 
-% Keine Ahnung wo die anderen herkamen?!
-TimeShifts = TimeShifts(1:20,1:8);
 %% Grid und Surfplot
 surf(X,Y,TimeShifts')
 title('Arithmetischer Mittelwert der Zeitdifferenz von Peaks in Abhaengigkeit von Frequenz und max Aktivierung');
